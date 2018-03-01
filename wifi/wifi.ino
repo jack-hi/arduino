bbf63cec010534acfa49ee0cf16afbd601c50541
jack-hi <hjj414@163.com>
2018-03-01 21:14:24 +0800

    Arduino UNO + ESP8266 wifi

---
+++ wifi/wifi.ino
@@ -0,0 +1,35 @@
+#include <SoftwareSerial.h>
+#include <SerialESP8266wifi.h>
+
+#define esp_uart_rx 2
+#define esp_uart_tx 3
+#define esp_rst_pin 4
+
+SoftwareSerial espSerial(esp_uart_rx, esp_uart_tx); //Rx Tx
+
+// init esp8266 wifi
+SerialESP8266wifi wifi(espSerial, espSerial, esp_rst_pin, Serial);
+
+void setup() {
+  
+  espSerial.begin(9600);
+
+  Serial.begin(115200);
+  while (!Serial)
+    ;
+  Serial.println("WiFi starting......");
+
+  wifi.begin();
+
+  wifi.connectToAP("TP-LINK_C2B3", "yy@212310429");
+  //wifi.connectToServer("192.168.2.105", "10001");
+  wifi.setTransportToUDP();
+  wifi.connectToServer("202.112.31.197", "123"); //NTP server
+
+  
+}
+
+void loop() {
+  //wifi.send(SERVER, "ESP8266 TEST");
+  //delay(3000);
+}

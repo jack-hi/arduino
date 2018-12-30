9029fd75b099570091e5137e94d8f52da2bdf9bb
jack <hjj414@gmail.com>
2018-12-30 19:34:46 +0800

    ESP8266 wifi tests

--- wifi/wifi.ino
+++ wifi/wifi.ino
@@ -1,7 +1,7 @@
 #include <SoftwareSerial.h>
 #include <SerialESP8266wifi.h>
 #include <Wire.h>
-#include "EEPROMAT24C256.h"
+//#include "EEPROMAT24C256.h"
 
 
 #define esp_uart_rx 2
@@ -11,8 +11,11 @@
 SoftwareSerial espSerial(esp_uart_rx, esp_uart_tx); //Rx Tx
 
 // init esp8266 wifi
-SerialESP8266wifi wifi(espSerial, espSerial, esp_rst_pin, Serial);
+//SerialESP8266wifi wifi(espSerial, espSerial, esp_rst_pin, Serial);
+SerialESP8266wifi wifi(espSerial, espSerial, esp_rst_pin);
 
+
+int ret = -1;
 
 void setup() {
   
@@ -27,10 +30,17 @@ void setup() {
   wifi.begin();
 
   wifi.connectToAP("TP-LINK_C2B3", "yy@212310429");
-  wifi.setTransportToUDP();
-  wifi.connectToServer("202.112.31.197", "123"); //NTP server
+  //wifi.setTransportToUDP();
+  //wifi.connectToServer("202.112.31.197", "123"); //NTP server
+  //delay(10000);
+  if (wifi.connectToServer("192.168.2.123", "41400")) {
+    Serial.println("\nConnected to Server ----------");
+    wifi.endSendWithNewline(false);
+  }
+ // wifi.send(SERVER, "ESP8266 TEST");
 #endif
 
+#if 0
   /* AT24C256 EEPROM, Size: 256KB, Device address 0x50(0xA0>>1) */
   Wire.begin();
   
@@ -38,10 +48,21 @@ void setup() {
   er.write_byte(0x0011, 0x11);  
   
   Serial.print(er.read_byte(0x0011), HEX);
+#endif
 
 }
 
 void loop() {
-  //wifi.send(SERVER, "ESP8266 TEST");
+  String time = String(millis(), DEC);
+  wifi.send(SERVER, time.c_str());
+
+  delay(20);
  
+  //WifiMessage in = wifi.listenForIncomingMessage(5000); //timeout 5s
+  WifiMessage in = wifi.getIncomingMessage();
+  if (in.hasData) {
+    Serial.println(in.channel);
+    Serial.println(in.message);  
+  }
+
 }
